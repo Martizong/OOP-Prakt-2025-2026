e18e91cd4351de450cd68dee8fc08d2aeb31d489
DanailIvanov21 <101013894+DanailIvanov21@users.noreply.github.com>
2026-04-21 17:09:37 +0300

    Create MyString.h

---
+++ Week 8/Solutions/task01/MyString.h
@@ -0,0 +1,42 @@
+#ifndef MYSTRING_H
+#define MYSTRING_H
+
+#include <cstring>
+#include <stdexcept>
+
+class MyString {
+private:
+    char* data;
+    size_t size;
+    size_t capacity;
+
+    void freeDynamic();
+    void copyDynamic(const char* source, size_t newSize);
+    void resize(size_t newCap);
+
+public:
+    MyString();
+    MyString(const char* str);
+    MyString(const MyString& other);
+    ~MyString();
+
+    MyString& operator=(const MyString& other);
+
+    char at(size_t index) const;
+    char front() const;
+    char back() const;
+    const char* str() const;
+    size_t length() const;
+    size_t getCapacity() const;
+    bool empty() const;
+
+    void append(char c);
+    void append(const char* str);
+    void append(const MyString& other);
+
+    void clear();
+    bool equals(const MyString& other) const;
+    int find(char c) const;
+};
+
+#endif
